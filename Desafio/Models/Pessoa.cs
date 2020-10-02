57545bd1cbc8742bccac2787352fa3140e407741
MatheusAFPimentel <matheus.algustt@gmail.com>
2020-10-02 20:19:09 -0300

    Arquivos do desafio

---
+++ Desafio/Models/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Desafio.Models
+{
+    public class Pessoa
+    {
+        [Key]
+        public int IdPessoa { get; set; }
+        public string nome { get; set; }
+        public DateTime dataNascimento { get; set; }
+        public float salario { get; set; }
+
+        public int numeroTelefone { get; set; }
+        public virtual Telefone telefone { get; set; }
+    }
+}
---
+++ Desafio/Models/Telefone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Desafio.Models
+{
+    public class Telefone
+    {
+        [Key]
+        public int idTelefone { get; set; }
+        public int numero { get; set; }
+    }
+}
